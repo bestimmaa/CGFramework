b4b01915ee135e643b50216bfd24825eb7781edd
Christoph Halang <christoph.halang@gmail.com>
2013-11-17 15:54:21 +0100

    added simple phong shading

--- src/shaders/simpleFragmentShader.fs
+++ src/shaders/simpleFragmentShader.fs
@@ -1,6 +1,7 @@
 #version 330
 
 in  vec4 normal;
+in  vec4 position;
 
 out vec4 out_Color;
 
@@ -8,7 +9,31 @@ out vec4 out_Color;
 
 void main(void)
 {
-	//in absence of any material, set everything just red
-    out_Color = normal;
+    vec4 baseColor = vec4(59/255.0,61/255.0,117/255.0,0);
+    
+    // we are in eye space here, the eye is at 0,0,0!
+    vec3 lightPosition = vec3(11.0,10.0,0.0);
+    vec4 AmbientColor = vec4(0.0, 0.0, 0.0, 1.0);
+    vec4 DiffuseColor = vec4(0.0, 0.0, 0.0, 1.0);
+    
+    vec3 vertexToLightVector =  normalize(lightPosition - position.xyz);
+    vec3 normalizedNormal = normalize(normal.xyz);
+    vec3 eyeToVertexVector = normalize(-reflect(vertexToLightVector,normalizedNormal));
+    vec3 eye = normalize(-vec3(position.xyz));
+    
+    int phongReflectionRadius = 90; // radius of the phong reflections
 
+    float ambientFactor = 0.2;
+    vec4 ambientTerm = ambientFactor*baseColor;
+
+    float diffuseFactor = max(dot(normalizedNormal,vertexToLightVector),0.0);
+    vec4 diffuseTerm = diffuseFactor*baseColor;
+    
+    
+    float shininess = 5;
+    float specularFactor = pow(max(dot(eyeToVertexVector,eye),0.0),shininess);
+    vec4 specularTerm = specularFactor * baseColor;
+    
+    out_Color = ambientTerm+diffuseTerm+specularTerm;
+    
 }
--- src/shaders/simpleVertexShader.vs
+++ src/shaders/simpleVertexShader.vs
@@ -1,18 +1,26 @@
 #version 330
 
+//Input for every vertex
 layout(location=0) in vec3 in_Position;
 layout(location=1) in vec3 in_Normal;
-out vec4 normal;
 
-//Matrix Uniforms as specified with glUniformMatrix4fv
+//Output
+out vec4 normal;
+out vec4 position;
+
+//Input for all vertices
 uniform mat4 ModelViewMatrix;
 uniform mat4 ProjectionMatrix;
 uniform mat4 NormalMatrix;
 
 
+
 void main(void)
 {
-	//^= old ModelViewProjectionMatrix
+	//Calculate the final vertex position
 	gl_Position = (ProjectionMatrix * ModelViewMatrix) * vec4(in_Position,1.0);
+    //Transform the normal and pass it to the fragment shader
 	normal      = normalize(NormalMatrix * vec4(in_Normal, 0.0));
+    //Tansform vertex position to eye space
+    position = ModelViewMatrix*vec4(in_Position,1.0);
 }
